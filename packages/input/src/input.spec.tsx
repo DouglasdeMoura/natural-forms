38544559decc38ff724e37ba985d99043a30770b
Douglas Moura <douglas.ademoura@gmail.com>
2022-02-19 12:58:45 -0300

    feat: add validation and custome error messages

--- packages/input/src/input.spec.tsx
+++ packages/input/src/input.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import * as InputPrimitive from '.'
 
@@ -35,5 +36,147 @@ describe('<InputPrimitive.* />', () => {
     const label = screen.getByText('mock_label')
 
     expect(label).toHaveAttribute('for', 'mock_id')
+  })
+
+  it('should validate a required input', () => {
+    render(<Input label="mock_label" error="This input is required" required />)
+
+    screen.getByLabelText('mock_label')
+
+    userEvent.tab()
+    userEvent.tab()
+
+    expect(screen.getByText('This input is required')).toBeInTheDocument()
+  })
+
+  it('should validate an input with a defined minLength', () => {
+    render(
+      <Input label="mock_label" error="The minLength is 5" minLength={5} />,
+    )
+
+    userEvent.type(screen.getByLabelText('mock_label'), '123')
+    userEvent.tab()
+
+    expect(screen.getByText('The minLength is 5')).toBeInTheDocument()
+  })
+
+  it('should validate an input with a defined maxLength', () => {
+    render(
+      <Input
+        label="mock_label"
+        error="The maxLength is 5"
+        defaultValue="1234567"
+        maxLength={5}
+      />,
+    )
+    const input = screen.getByLabelText('mock_label') as HTMLInputElement
+    userEvent.type(input, '{backspace}')
+    userEvent.tab()
+
+    expect(screen.getByText('The maxLength is 5')).toBeInTheDocument()
+  })
+
+  it('should validate a pattern', () => {
+    const handleOnBlur = jest.fn()
+
+    render(
+      <Input
+        label="mock_label"
+        error="You must enter three numbers"
+        onBlur={handleOnBlur}
+        pattern={'[0-9]{3}'}
+      />,
+    )
+
+    const input = screen.getByLabelText('mock_label') as HTMLInputElement
+
+    userEvent.type(input, '1234567')
+    userEvent.tab()
+
+    expect(handleOnBlur).toHaveBeenCalledTimes(1)
+    expect(screen.getByText('You must enter three numbers')).toBeInTheDocument()
+  })
+
+  it('should validate a min and max range', () => {
+    const handleOnBlur = jest.fn()
+
+    render(
+      <Input
+        label="mock_label"
+        error="You must enter a number between 3 and 5"
+        onBlur={handleOnBlur}
+        min={3}
+        max={5}
+      />,
+    )
+
+    userEvent.type(screen.getByLabelText('mock_label'), '1')
+    userEvent.tab()
+
+    expect(handleOnBlur).toHaveBeenCalledTimes(1)
+    expect(
+      screen.getByText('You must enter a number between 3 and 5'),
+    ).toBeInTheDocument()
+
+    userEvent.type(screen.getByLabelText('mock_label'), '5')
+    userEvent.tab()
+
+    expect(
+      screen.getByText('You must enter a number between 3 and 5'),
+    ).toBeInTheDocument()
+  })
+
+  it('should validate a custom function', () => {
+    const customvalidator = jest.fn().mockReturnValue(false)
+
+    render(
+      <Input
+        label="mock_label"
+        error="Custom error"
+        custom={customvalidator}
+      />,
+    )
+
+    userEvent.type(screen.getByLabelText('mock_label'), '5')
+    userEvent.tab()
+
+    expect(customvalidator).toHaveBeenCalledWith('5')
+    expect(screen.getByText('Custom error')).toBeInTheDocument()
+  })
+
+  it('should call the onValidate handler when the input is valid', () => {
+    const onValidate = jest.fn()
+
+    render(
+      <Input
+        label="mock_label"
+        error="Required"
+        onValidate={onValidate}
+        required
+      />,
+    )
+
+    userEvent.type(screen.getByLabelText('mock_label'), '1')
+    userEvent.tab()
+
+    expect(onValidate).toHaveBeenCalledWith('1')
+  })
+
+  it('should call the onError handler when the input is invalid', () => {
+    const onError = jest.fn()
+
+    render(
+      <Input
+        label="mock_label"
+        error="Required"
+        onError={onError}
+        type="email"
+      />,
+    )
+
+    userEvent.type(screen.getByLabelText('mock_label'), '1')
+    userEvent.tab()
+
+    expect(onError).toHaveBeenCalled()
   })
 })
--- packages/input/src/input.tsx
+++ packages/input/src/input.tsx
@@ -1,35 +1,225 @@
-import { forwardRef, useEffect } from 'react'
+import { forwardRef, useEffect, useRef } from 'react'
+import { createContext, useContext, useState } from 'react'
 
-import { Root as RootComponent, useFieldContext } from '@natural-forms/context'
-import { Error as ErrorComponent } from '@natural-forms/error'
-import { Label as LabelComponent } from '@natural-forms/label'
+import useMergedRef from '@react-hook/merged-ref'
 import { camelCase } from 'lodash'
 
+/**
+ * Validation
+ */
+
+type Errors = {
+  type?: string
+  minLength?: string
+  maxLength?: string
+  min?: string
+  max?: string
+  pattern?: string
+  required?: string
+  custom?: string
+}
+
+type ErrorMessages = string | Errors
+
+type ValidationResult = {
+  valid: boolean
+  errors?: {
+    type: keyof Errors
+    message: string
+  }[]
+}
+
+function validate(
+  props: Pick<
+    InputProps,
+    | 'required'
+    | 'minLength'
+    | 'maxLength'
+    | 'pattern'
+    | 'type'
+    | 'min'
+    | 'max'
+    | 'custom'
+  >,
+  element: React.RefObject<HTMLInputElement>,
+  messages?: ErrorMessages,
+): ValidationResult {
+  const validators = {
+    required: element.current?.validity.valueMissing,
+    minLength:
+      element.current?.validity.tooShort ||
+      (props?.minLength && element!.current!.value.length < props.minLength),
+    maxLength:
+      element.current?.validity.tooLong ||
+      (props?.maxLength && element!.current!.value.length > props.maxLength),
+    pattern: element.current?.validity.patternMismatch,
+    type: element.current?.validity.typeMismatch,
+    min:
+      element.current?.validity.rangeUnderflow ||
+      (props?.min && Number(element!.current!.value) < props.min),
+    max:
+      element.current?.validity.rangeOverflow ||
+      (props?.max && Number(element!.current!.value) > props.max),
+    custom: props?.custom && !props.custom?.(element!.current!.value),
+  }
+
+  const errors: ValidationResult['errors'] = []
+
+  for (const [validation, invalid] of Object.entries(validators)) {
+    if (invalid) {
+      errors.push({
+        type: validation as keyof Errors,
+        message:
+          (typeof messages === 'object'
+            ? messages[validation as keyof Errors]
+            : messages) || '',
+      })
+    }
+  }
+
+  if (errors.length > 0) {
+    return {
+      valid: false,
+      errors,
+    }
+  }
+
+  return { valid: true }
+}
+
+/**
+ * Create context for the input
+ */
+
+type FieldContextData = {
+  id: string
+  label: string
+  errors: ValidationResult['errors'] | null
+  setId: (id: string) => void
+  setLabel: (label: string) => void
+  setErrors: (errors?: ValidationResult['errors']) => void
+}
+
+type RootProps = {
+  children: React.ReactNode
+}
+
+const FieldContext = createContext({} as FieldContextData)
+
+export const Root: React.FC<RootProps> = ({ children }) => {
+  const [id, setId] = useState('')
+  const [label, setLabel] = useState('')
+  const [errors, setErrors] = useState<ValidationResult['errors'] | undefined>()
+
+  return (
+    <FieldContext.Provider
+      value={{
+        id,
+        label,
+        errors,
+        setId,
+        setLabel,
+        setErrors,
+      }}
+    >
+      {children}
+    </FieldContext.Provider>
+  )
+}
+
+export const useFieldContext = () => useContext(FieldContext)
+
+/**
+ * Container to display the error message
+ */
+
+type ErrorProps = React.HTMLAttributes<HTMLParagraphElement>
+
+export const Error: React.FC<ErrorProps> = ({ ...props }) => {
+  const { errors } = useFieldContext()
+
+  return errors ? <p {...props}>{errors[0].message}</p> : null
+}
+
+/**
+ * Container to display the error message
+ */
+
+type LabelProps = {
+  children: React.ReactNode
+} & React.HTMLAttributes<HTMLLabelElement>
+
+export const Label: React.FC<LabelProps> = ({ children, ...props }) => {
+  const { id, setLabel } = useFieldContext()
+
+  useEffect(() => {
+    setLabel(children?.toString() || '')
+  }, [children, setLabel])
+
+  return (
+    <label htmlFor={id} {...props}>
+      {children}
+    </label>
+  )
+}
+
+/**
+ * The input component
+ */
+
 type InputProps = {
+  error?: ErrorMessages
   custom?: (value: string) => boolean
-  onError?: (name: string) => void
+  onError?: (value: string, errors: ValidationResult['errors']) => void
   onValidate?: (value?: string) => boolean
 } & React.ComponentPropsWithRef<'input'>
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ ...props }, ref) => {
-    const { setId, label } = useFieldContext()
+  ({ custom, error, onBlur, onChange, onError, onValidate, ...props }, ref) => {
+    const { setId, label, setErrors, errors } = useFieldContext()
+
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const id = props?.id || camelCase(label)
     const name = props?.name || id
+
+    const handleOnBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+      onBlur?.(e)
+
+      const validation = validate({ custom, ...props }, inputRef, error)
+
+      if (!validation.valid) {
+        setErrors(validation.errors)
+        onError?.(inputRef.current?.value || '', errors || [])
+        return
+      }
+
+      onValidate?.(inputRef.current?.value)
+    }
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange?.(e)
+
+      if (errors) {
+        setErrors(undefined)
+      }
+    }
 
     useEffect(() => {
       setId(id)
     }, [id, setId])
 
-    return <input {...props} id={id} name={name} ref={ref} />
+    return (
+      <input
+        {...props}
+        id={id}
+        name={name}
+        onBlur={handleOnBlur}
+        onChange={handleOnChange}
+        ref={useMergedRef(ref, inputRef)}
+      />
+    )
   },
 )
 
 Input.displayName = 'Input'
-
-export const Label = LabelComponent
-
-export const Error = ErrorComponent
-
-export const Root = RootComponent
